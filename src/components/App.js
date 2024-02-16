14a0b905f79fd86f1bcb0895b10f957a69cedbbe
gamze <gamzekandara@gmail.com>
2024-02-16 01:00:06 -0500

    Adds App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [plates, setPlates] = useState([]);
+  const [balance, setBalance] = useState(100);
+
+  const updateBalance = plates.map((plate) => {
+    if (balance > plate.price)
+      return setBalance((balance) => balance - plate.price);
+  });
+
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer plates={plates} setPlates={setPlates} />
+      <Table plates={plates} balance={balance} />
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ setOnStack, onStack, plates, setPlates }) {
+  const [showSushis, setShowSushis] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/sushis")
+      .then((r) => r.json())
+      .then((data) => setShowSushis(data));
+  }, []);
+
+  const sushiPlates = showSushis
+    .slice(currentIndex, currentIndex + 4)
+    .map((sushi) => (
+      <Sushi
+        key={sushi.id}
+        id={sushi.id}
+        sushi={sushi}
+        plates={plates}
+        setPlates={setPlates}
+      />
+    ));
+
+  const incrementIndex = () => {
+    setCurrentIndex((currentIndex) => currentIndex + 4);
+  };
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiPlates}
+      <MoreButton incrementIndex={incrementIndex} />
     </div>
   );
 }
